e441b951b218af3b11a5e4db68163455c477fa2e
Ramon <ramm050489@gmail.com>
2020-12-17 22:13:40 -0500

    commit tree 86f2f17d16c1331d68251256a231c6b21dc0a44a

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,21 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash)
+#Build a method key_for_min_value that accepts an argument of a hash. 
+#This method should iterate over the hash and return the key (not the value!) 
+#that points to the smallest value of the set. 
+#If the method is called and passed an argument of an empty hash, it should return nil.
 
+
+
+def key_for_min_value(name_hash)
+    low_key = nil
+    low_value = Float::INFINITY
+    name_hash.each do |key, value|
+        if value <low_value
+            low_value = value
+            low_key = key
+        end
+    end
+    low_key
 end
